e25d9c58ebbf46b8e2d2c29856c04d1b954905c0
CAIQUE <CAIQUE@C-ALVES>
2021-07-13 17:09:19 -0300

    Add project files.

---
+++ StructsMediaProdutos/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructsMediaProdutos
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ StructsMediaProdutos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace StructsMediaProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos Produtos ?");
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];//Vetor Tipo Struct
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nome do Produto : ");
+                string name = Console.ReadLine();
+                Console.Write("Preço do Produto :$");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Nome = name, Price = price };//Instanciação sem Construtor
+
+
+            }
+            double soma = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i].Price;//.Price indica a Posição que desejo mecher no vetor. Ex: no Preço, .Name Meche no Nome e sem Indicação usa como um Todo!
+            }
+            double avg = soma / n;
+            Console.WriteLine();
+            Console.WriteLine("Média de Valor : $" + avg.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
